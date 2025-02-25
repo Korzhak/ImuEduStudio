94739113bfa2a1202ea6c0b6d2ba552b6c616903
Korzhak <bohdan.korzhak@gmail.com>
2025-02-25 09:08:57 +0200

    added pyqtier dock widget

--- app/widgets/main_window.py
+++ app/widgets/main_window.py
@@ -10,6 +10,10 @@ from app.widgets.tree_axis_plot_widget import (
     RotatorAngleWidget,
     RotatorSpeedWidget
 )
+import pyqtgraph as pg
+
+from pyqtgraph.dockarea.Dock import Dock
+from pyqtgraph.dockarea.DockArea import DockArea
 
 
 class MainWindow(PyQtierMainWindow):
@@ -46,37 +50,47 @@ class MainWindow(PyQtierMainWindow):
         print(data)
 
     def create_plots(self):
-        self.rotator_angle_widget = RotatorAngleWidget()
-        self.rotator_speed_widget = RotatorSpeedWidget()
+        self.dock_area = DockArea()
+        self.setCentralWidget(self.dock_area)
 
+        self.rotator_speed_widget = RotatorSpeedWidget()
+        self.rotator_angle_widget = RotatorAngleWidget()
         self.gyroscope_widget = GyroscopeWidget()
         self.accelerometer_widget = AccelerometerWidget()
         self.euler_angles_widget = EulerAnglesWidget()
 
-        self.rotator_angle_layout = QHBoxLayout()
-        self.rotator_angle_layout.setContentsMargins(0, 0, 0, 0)
-        self.view.rotation_angle_widget.setLayout(self.rotator_angle_layout)
-        self.rotator_angle_layout.addWidget(self.rotator_angle_widget)
+        self.angular_rate_dock = Dock("Кутова швидкість", size=(500, 300))
+        self.accel_dock = Dock("Кутова швидкість", size=(500, 300))
 
-        self.rotator_speed_layout = QHBoxLayout()
-        self.rotator_speed_layout.setContentsMargins(0, 0, 0, 0)
-        self.view.rotation_speed_widget.setLayout(self.rotator_speed_layout)
-        self.rotator_speed_layout.addWidget(self.rotator_speed_widget)
+        self.angular_rate_dock.addWidget(self.gyroscope_widget)
+        self.accel_dock.addWidget(self.accelerometer_widget)
 
-        self.accel_plot_layout = QHBoxLayout()
-        self.accel_plot_layout.setContentsMargins(0, 0, 0, 0)
-        self.view.accel_plot_widget.setLayout(self.accel_plot_layout)
-        self.accel_plot_layout.addWidget(self.accelerometer_widget)
-
-        self.gyro_plot_layout = QHBoxLayout()
-        self.gyro_plot_layout.setContentsMargins(0, 0, 0, 0)
-        self.view.gyro_plot_widget.setLayout(self.gyro_plot_layout)
-        self.gyro_plot_layout.addWidget(self.gyroscope_widget)
-
-        self.euler_angles_plot_layout = QHBoxLayout()
-        self.euler_angles_plot_layout.setContentsMargins(0, 0, 0, 0)
-        self.view.euler_plot_widget.setLayout(self.euler_angles_plot_layout)
-        self.euler_angles_plot_layout.addWidget(self.euler_angles_widget)
+        self.dock_area.addDock(self.angular_rate_dock)
+        self.dock_area.addDock(self.accel_dock)
+        # self.rotator_angle_layout = QHBoxLayout()
+        # self.rotator_angle_layout.setContentsMargins(0, 0, 0, 0)
+        # self.view.rotation_angle_widget.setLayout(self.rotator_angle_layout)
+        # self.rotator_angle_layout.addWidget(self.rotator_angle_widget)
+        #
+        # self.rotator_speed_layout = QHBoxLayout()
+        # self.rotator_speed_layout.setContentsMargins(0, 0, 0, 0)
+        # self.view.rotation_speed_widget.setLayout(self.rotator_speed_layout)
+        # self.rotator_speed_layout.addWidget(self.rotator_speed_widget)
+        #
+        # self.accel_plot_layout = QHBoxLayout()
+        # self.accel_plot_layout.setContentsMargins(0, 0, 0, 0)
+        # self.view.accel_plot_widget.setLayout(self.accel_plot_layout)
+        # self.accel_plot_layout.addWidget(self.accelerometer_widget)
+        #
+        # self.gyro_plot_layout = QHBoxLayout()
+        # self.gyro_plot_layout.setContentsMargins(0, 0, 0, 0)
+        # self.view.gyro_plot_widget.setLayout(self.gyro_plot_layout)
+        # self.gyro_plot_layout.addWidget(self.gyroscope_widget)
+        #
+        # self.euler_angles_plot_layout = QHBoxLayout()
+        # self.euler_angles_plot_layout.setContentsMargins(0, 0, 0, 0)
+        # self.view.euler_plot_widget.setLayout(self.euler_angles_plot_layout)
+        # self.euler_angles_plot_layout.addWidget(self.euler_angles_widget)
 
     def create_menu(self):
         # Створюємо меню "Вигляд"
